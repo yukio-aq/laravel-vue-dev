f8f9b1cd84b0e679dda4e52ddfff2aab4da0b3af
kazuyuki-hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-10-31 12:02:45 +0900

    add Tasks delete API

--- laravel9-vue3/app/Http/Controllers/TaskController.php
+++ laravel9-vue3/app/Http/Controllers/TaskController.php
@@ -29,4 +29,11 @@ class TaskController extends Controller
 
         return $task;
     }
+
+    public function destroy(Task $task): Task
+    {
+        $task->delete();
+
+        return $task;
+    }
 }
--- laravel9-vue3/routes/api.php
+++ laravel9-vue3/routes/api.php
@@ -23,4 +23,5 @@ Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::get('/tasks/{task}', [TaskController::class, 'show']);
 Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 
