b9a0c588f4807bf012188d67eafa98f6f61c4a04
kazuyuki-hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-10-31 11:46:40 +0900

    add Tasks index API

---
+++ laravel9-vue3/app/Http/Controllers/TaskController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use \Illuminate\Database\Eloquent\Collection;
+
+class TaskController extends Controller
+{
+    public function index(): Collection
+    {
+        return Task::all();
+    }
+}
