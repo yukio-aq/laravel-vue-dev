b2770591f63bcca1f3c47e7561761644ad00920e
kazuyuki-hosoda <kazuyuki.hosoda@asia-quest.jp>
2022-10-31 10:32:37 +0900

    add TaskCreate-component

--- laravel9-vue3/resources/router.js
+++ laravel9-vue3/resources/router.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory} from "vue-router";
 import TaskListComponent from "./js/components/TaskListComponent.vue";
 import TaskShowComponent from "./js/components/TaskShowComponent.vue";
+import TaskCreateComponent from "./js/components/TaskCreateComponent.vue";
 
 const routes = [
     {
         path: '/tasks',
         name: 'task.list',
         component: TaskListComponent
+    },
+    {
+        path: '/tasks/create',
+        name: 'task.create',
+        component: TaskCreateComponent
     },
     {
         path: '/tasks/:taskId',
